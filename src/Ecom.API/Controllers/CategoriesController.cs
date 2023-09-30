7e3da30fc36269ee1d82a6633004bbfd787334a0
ali <ali.aboelazaym@gmail.com>
2023-09-30 21:13:31 +0300

    Add Categories Controller

---
+++ src/Ecom.API/Controllers/CategoriesController.cs
@@ -0,0 +1,29 @@
+﻿using Ecom.Core.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Ecom.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Getallascyn : ControllerBase
+    {
+        private readonly IUnitOfWork _uow;
+
+        public Getallascyn(IUnitOfWork uow)
+        {
+            _uow = uow;
+        }
+
+        public async Task <ActionResult> GetAllAsync()
+        {
+            var allcategory= await _uow.CategoryRepository.GetAllAsync();
+            if (allcategory is not null)
+            {
+                return Ok(allcategory);
+            }
+            return BadRequest("Not Found");
+        }
+    }
+}
